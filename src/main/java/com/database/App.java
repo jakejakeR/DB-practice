2738b3298c51849aceb6cbac0b4dca6b5b677db4
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-20 16:54:06 +0200

    Connection to DB

--- src/main/java/com/database/App.java
+++ src/main/java/com/database/App.java
@@ -1,13 +1,24 @@
 package com.database;
 
+import com.database.privateData.Directory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+
+        try {
+            Connection dbConnection = DriverManager.getConnection(Directory.DB_DIRECTORY);
+
+        } catch(SQLException ex) {
+            System.out.println("Something went wrong: " + ex.getMessage());
+        }
+
     }
 }
